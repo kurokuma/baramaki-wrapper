b11aede0688974f592929b74e259ea7a304f54c8
ranran <kurokuma@users.noreply.github.com>
2020-10-25 03:40:20 +0900

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from baramaki.baramaki_table import BaramakiTable
+
+def main():
+    b = BaramakiTable()
+    # print(b.get_domain())
+    # print(b.get_hash())
+    # print(b.get_ip())
+    # print(b.get_url())
+    # print(b.get_task())
+
+
+if __name__ == "__main__":
+    main()
